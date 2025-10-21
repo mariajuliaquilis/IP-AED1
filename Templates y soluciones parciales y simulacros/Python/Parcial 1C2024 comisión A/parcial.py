f549f8711399ab83ad75a976839f52058f35e2c0
mariajuliaquilis <quilismariajulia@hotmail.com>
2025-10-21 19:48:45 -0300

    Subo templates y soluciones de parciales de otros cuatrimestres

---
+++ Templates y soluciones parciales y simulacros/Python/Parcial 1C2024 comisión A/parcial.py
@@ -0,0 +1,13 @@
+from queue import Queue as Cola
+
+#Ejercicio 1
+def orden_de_atencion (urgentes: Cola[int], postergables: Cola[int])->Cola[int]:
+
+#Ejercicio 2
+def alarma_epidemiologica(registros: list[tuple[int, str]], infecciosas: list[str], umbral: float) -> dict[str, float]:
+
+#Ejercicio 3
+def empleados_del_mes(horas: dict[int, list[int]]) -> list[int]:
+
+#Ejercicio 4
+def nivel_de_ocupacion(pisos: list[list[bool]]) -> list[float]:
