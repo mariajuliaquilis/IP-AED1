c8b90dceb7982f335ee506cdf0381d9dc221451d
mariajuliaquilis <quilismariajulia@hotmail.com>
2025-11-11 23:36:04 -0300

    Subo parciales y simulacros de haskell para hacerlos

---
+++ Parciales y simulacros/Haskell/Parcial 2C2023 TM/parcial.hs
@@ -0,0 +1,17 @@
+module Solucion where
+
+-- Ejercicio 1
+votosEnBlanco :: [(String, String)] -> [Int] -> Int  -> Int
+votosEnBlanco _ _ _ = 0
+
+-- Ejercicio 2
+formulasValidas :: [(String, String)] -> Bool
+formulasValidas _ = True
+
+-- Ejercicio 3
+porcentajeDeVotos :: String -> [(String, String)] -> [Int] -> Float
+porcentajeDeVotos _ _ _ = 0.0
+
+-- Ejercicio 4
+proximoPresidente :: [(String, String)] -> [Int] -> String
+proximoPresidente _ _ = ""
