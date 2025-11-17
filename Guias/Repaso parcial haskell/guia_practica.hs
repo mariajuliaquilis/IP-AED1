23c28e80e8f3535ccd1668a94d827bbc9577c0cc
mariajuliaquilis <quilismariajulia@hotmail.com>
2025-11-17 01:18:11 -0300

    Terminé la parte de Sistema de stock

--- Guias/Repaso parcial haskell/guia_practica.hs
+++ Guias/Repaso parcial haskell/guia_practica.hs
@@ -61,6 +61,50 @@ stockDeProducto stock producto | not(pertenece (listaDeProductos stock) producto
                                | otherwise = stockDeProducto (tail stock) producto                 
 
 
+{-Ejercicio 3. Implementar la función dineroEnStock :: [(String, Int))] -> [(String, Float)] -> Float
+problema dineroEnStock (stock: seq⟨String ×Z⟩, precios: seq⟨String ×R⟩ ) : R {
+    requiere: {No existen dos nombres de productos (primeras componentes) iguales en stock}
+    requiere: {No existen dos nombres de productos (primeras componentes) iguales en precios}
+    requiere: {Todas las cantidades (segundas componentes) de stock son mayores a cero}
+    requiere: {Todos los precios (segundas componentes) de precios son mayores a cero}
+    requiere: {Todo producto de stock aparece en la lista de precios}
+    asegura: {res es igual a la suma de los precios de todos los productos que están en stock multiplicado por la cantidad
+    de cada producto que hay en stock}
+}
+Para resolver este ejercicio pueden utilizar la función del Preludio de Haskell fromIntegral que dado un valor de tipo
+Int devuelve su equivalente de tipo Float.
+-}
+sumaPrecios :: [(String, Float)] -> Float
+sumaPrecios precios | longitud(precios) == 0 = 0
+                    | otherwise = (snd(head precios)) + sumaPrecios (tail precios)
+
+cantidadDeProductos :: [(String, Int)] -> Int
+cantidadDeProductos productos | longitud(productos) == 0 = 0
+                              | otherwise = (snd(head productos)) + cantidadDeProductos (tail productos)  
+
+dineroEnStock :: [(String, Int)] -> [(String, Float)] -> Float
+dineroEnStock stock precios = sumaPrecios(precios)*(fromIntegral(cantidadDeProductos(stock)))
+
+{-Ejercicio 4. 
+Implementar la función aplicarOferta :: [(String, Int)] -> [(String, Float)] -> [(String,Float)]
+problema aplicarOferta (stock: seq⟨String ×Z⟩, precios: seq⟨String ×R⟩ ) : seq⟨String × R⟩ {
+    requiere: {No existen dos nombres de productos (primeras componentes) iguales en stock}
+    requiere: {No existen dos nombres de productos (primeras componentes) iguales en precios}
+    requiere: {Todas las cantidades (segundas componentes) de stock son mayores a cero}
+    requiere: {Todos los precios (segundas componentes) de precios son mayores a cero}
+    requiere: {Todo producto de stock aparece en la lista de precios}
+    asegura: {|res|= |precios|}
+    asegura: {Para todo 0 ≤i < |precios|, si stockDeProducto(stock, precios[i]_0) > 10, entonces res[i]_0 = precios[i]_0 y
+              res[i]_1 = precios[i]_1∗ 0,80}
+    asegura: {Para todo 0 ≤i < |precios|, si stockDeProducto(stock, precios[i]_0) ≤ 10, entonces res[i]_0 = precios[i]_0 y
+              res[i]_1 = precios[i]_1}
+}
+-}
+aplicarOferta :: [(String, Int)] -> [(String, Float)] -> [(String, Float)]
+aplicarOferta stock precios | longitud(precios) == 0 = []
+                            | (stockDeProducto stock (fst(head precios))) > 10 = (fst(head precios), (snd(head precios))*0.80):(aplicarOferta stock (tail precios)) 
+                            | otherwise = (fst(head precios), snd(head precios)):(aplicarOferta stock (tail precios))        
+
 {-Perfectos amigos
 El Departamento de Matemática (DM) de la FCEyN-UBA nos ha encargado que desarrollemos un sistema para el tratamiento de números naturales.
 Específicamente les interesa conocer cuándo un número es perfecto y cuándo dos números son amigos. Aunque por ahí no lo sabías, estos 
